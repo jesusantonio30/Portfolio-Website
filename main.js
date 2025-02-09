ef2b5fa16f1ed13884d226dacb1f5e9ee7dcd558
jesusantonio30 <morales.jesusant@gmail.com>
2025-02-09 15:31:35 -0800

    Added hover and active effects to buttons

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-// Retrieve the current dark mode status from localStorage
-let darkmode = localStorage.getItem('darkmode');
+// Retrieve the current light mode status from localStorage
+let lightmode = localStorage.getItem('lightmode');
 
 // Select the toggle button element
 const toggle = document.getElementById('nav-light-toggle');
 
 
-// Function to enable dark mode
-const enableDarkmode = () => {
-    document.body.classList.add('darkmode');
-    localStorage.setItem('darkmode', 'active');
+// Function to enable light mode
+const enableLightmode = () => {
+    document.body.classList.add('lightmode');
+    localStorage.setItem('lightmode', 'active');
 };
 
-// Function to disable dark mode
-const disableDarkmode = () => {
-    document.body.classList.remove('darkmode');
-    localStorage.setItem('darkmode', 'inactive');
+// Function to disable light mode
+const disableLightmode = () => {
+    document.body.classList.remove('lightmode');
+    localStorage.setItem('lightmode', 'inactive');
 };
 
 // Initialize the theme based on localStorage value
-if(darkmode === 'active') enableDarkmode();
+if(lightmode === 'active') enableLightmode();
 
 // Add event listener to the toggle button
 toggle.addEventListener('click', ()=>{
-    darkmode = localStorage.getItem('darkmode');
-    darkmode !== 'active' ? enableDarkmode() : disableDarkmode();
+    lightmode = localStorage.getItem('lightmode');
+    lightmode !== 'active' ? enableLightmode() : disableLightmode();
 })
 
